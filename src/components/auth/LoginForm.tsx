1e46876e60ddf16139833a2b355fb8a72c8ca57a
Flo-Foui <florian.fouilleul72@gmail.com>
2025-03-03 17:00:15 +0100

    Login + register

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -3,24 +3,13 @@ import {useNavigate} from "react-router";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {AuthContext} from "../../context/AuthContext";
 import {LoginFormType} from "../../1_types/LoginFormType";
-import axios from "axios";
 import "../../styles/loginForm.css"
-
-export const post = async (url: string, data: {}, config?: {}) => {
-    try {
-        const response = await axios.post(url, data, config);
-        console.log("Response", response);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
+import {post} from "../../api/post";
 
 const LoginForm: FC<{}> = ({}) => {
     const navigate = useNavigate();
     const authContext = useContext(AuthContext);
-    const {register, handleSubmit, formState: {errors}} = useForm<LoginFormType>({
+    const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<LoginFormType>({
         defaultValues: {
             email: "",
             password: "",
@@ -36,36 +25,39 @@ const LoginForm: FC<{}> = ({}) => {
 
             if (response) {
                 authContext?.setIsLoggedIn(true);
-                authContext?.setToken(response.data);
-                navigate("/");
+                authContext?.setToken(response);
+                navigate("/Dashboard");
             } else {
                 setErrorMessage("Identifiants incorrects !");
             }
         } catch (error) {
-            setErrorMessage("Une erreur est survenue !");
+            setErrorMessage("Identifiants incorrects ou erreur serveur !");
             console.error("Erreur lors de la connexion", error);
         }
     };
 
     return (
-            <div className="login-card">
-                {errorMessage && <p className="error-message">{errorMessage}</p>}
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="input-group">
-                        <label>Email</label>
-                        <input type="email" {...register("email", {required: "Email requis"})} />
-                        {errors.email && <p className="error-text">{errors.email.message}</p>}
-                    </div>
-                    <div className="input-group">
-                        <label>Mot de passe</label>
-                        <input type="password" {...register("password", {required: "Mot de passe requis"})} />
-                        {errors.password && <p className="error-text">{errors.password.message}</p>}
-                    </div>
-                    <button type="submit" className="login-button">Se connecter</button>
-                </form>
-                <p className="register-link" onClick={() => navigate("/Register")}>
-                    Pas encore de compte ? Cliquez ici pour vous inscrire !
-                </p>
+        <div className="login-card">
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="input-group">
+                    <label>Email</label>
+                    <input type="email" {...register("email", {required: "Email requis"})} />
+                    {errors.email && <p className="error-text">{errors.email.message}</p>}
+                </div>
+                <div className="input-group">
+                    <label>Mot de passe</label>
+                    <input type="password" {...register("password", {required: "Mot de passe requis"})} />
+                    {errors.password && <p className="error-text">{errors.password.message}</p>}
+                </div>
+                <button type="submit" className="login-button"
+                        disabled={isSubmitting}> {/*"disabled" évite le spam du bouton*/}
+                    Se connecter
+                </button>
+            </form>
+           {/* <p className="register-link" onClick={() => navigate("/Register")}>
+                Pas encore de compte ? Cliquez ici pour vous inscrire !
+            </p>*/}
         </div>
     );
 };
--- src/components/button/DisplayCardOrItem.tsx
+++ src/components/button/DisplayCardOrItem.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import {FC, useContext} from 'react';
 import {Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
