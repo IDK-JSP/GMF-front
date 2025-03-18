f18a48d5f03da6d40b592648159d8a7e79246798
Flo-Foui <florian.fouilleul72@gmail.com>
2025-03-18 14:52:35 +0100

    refacto

---
+++ src/1_types/CreateRecipeType.ts
@@ -0,0 +1,36 @@
+export interface Ingredient {
+    id_ingredient: number;
+    name: string;
+}
+
+export interface Measurement {
+    id_measurement: number;
+    name: string;
+    type: string;
+}
+
+export interface RecipeIngredient {
+    id_ingredient: number;
+    quantity: number;
+    id_measurement: number;
+}
+
+export interface Stage {
+    stage: number;
+    content: string;
+}
+
+export interface IngredientsSectionProps {
+    ingredients: RecipeIngredient[];
+    addIngredient: () => void;
+    updateIngredient: (index: number, field: keyof RecipeIngredient, value: number) => void;
+    allIngredients: Ingredient[];
+    allMeasurements: Measurement[];
+}
+
+export interface StepsSectionProps {
+    steps: string[];
+    addStep: () => void;
+    updateStep: (index: number, value: string) => void;
+    removeStep: (index: number) => void;
+}
---
+++ src/components/createRecipe/StepsSection.tsx
@@ -0,0 +1,26 @@
+import {Typography} from '@mui/material';
+import {FC} from 'react';
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const StepsSection: FC<StepsSectionProps> = ({steps, addStep, updateStep, removeStep}) => {
+
+    return (
+        <div>
+            <h3 className="section-title">Étapes</h3>
+            {steps.map((step, index) => (
+                <div key={index} className="step-container">
+                    <Typography sx={{fontSize: "48"}}>{index + 1}</Typography>
+                    <textarea className="input-field step-input" value={step}
+                              onChange={(e) => updateStep(index, e.target.value)}
+                              placeholder={`Étape ${index + 1}`} required/>
+                    <button type="button" className="btn-delete" onClick={() => removeStep(index)}>
+                        <DeleteIcon/>
+                    </button>
+                </div>
+            ))}
+            <button type="button" className="btn-add" onClick={addStep}>Ajouter une étape</button>
+        </div>
+    );
+};
+
+export default StepsSection;
