2adc15671f08e674da0d3b4fa9850061f539607c
Flo-Foui <florian.fouilleul72@gmail.com>
2025-03-19 13:48:08 +0100

    error page v1

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,33 @@
 import {FC} from 'react';
+import ContentWithoutAside from "../components/layout/ContentWithoutAside";
+import {Typography} from "@mui/material";
+import {useNavigate} from "react-router";
+import "../styles/errorPage.css"
 
 const ErrorPage: FC<{}> = ({}) => {
+    const navigate = useNavigate()
+
     return (
         <>
-            <h3>Y a une erreur là mon reuf</h3>
+            <main>
+                <ContentWithoutAside>
+                    <section>
+                        <article>
+                        <img src={"/404.png"} alt={"Erreur 404"}/>
+                            <Typography variant="h5">
+                                Oops ! La page que vous avez demandée n'existe pas...
+                            </Typography>
+                            <button className="home-btn" onClick={() => {
+                                navigate("/Home")
+                            }}>
+                            <Typography variant="h5">
+                                Revenir à l'accueil
+                            </Typography>
+                            </button>
+                        </article>
+                    </section>
+                </ContentWithoutAside>
+            </main>
         </>
     );
 };
