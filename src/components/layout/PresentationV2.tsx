6612555e7929914a9428ac487e1d161bdb018a82
Gabrielloop <gabriel.henin@gmail.com>
2025-03-20 09:23:40 +0100

    Enhance PresentationRecipe component: make recipeRate and recipeNbRate optional; add carousel and pageTitle props for improved flexibility

--- src/components/layout/PresentationV2.tsx
+++ src/components/layout/PresentationV2.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Children } from "../../1_types/PropsType";
 import OpinionRecipe from "../recipeDetails/OpinionRecipe";
+import { RecipeType } from "../../1_types/RecipeType";
 
 interface PresentationProps extends Children {
   imgUrl: string;
-  recipeRate: number;
-  recipeNbRate: number;
+  recipeRate?: number;
+  recipeNbRate?: number;
+  carousel?: boolean;
+  carouselCollection?: RecipeType[];
+  pageTitle?: string;
 }
 
-const PresentationRecipe: React.FC<PresentationProps> = ({ children, imgUrl, recipeNbRate, recipeRate }) => {
+const PresentationRecipe: React.FC<PresentationProps> = ({
+  children,
+  imgUrl,
+  recipeNbRate,
+  recipeRate,
+}) => {
   return (
     <div
       className="presentation"
@@ -20,9 +29,11 @@ const PresentationRecipe: React.FC<PresentationProps> = ({ children, imgUrl, rec
         width: "100%",
       }}
     >
-        <div>
-          <div className="presentation-titre">{children}</div>
-          <div className="presentation-aside"><OpinionRecipe recipeRate={recipeRate} recipeNbRate={recipeNbRate} /></div>
+      <div>
+        <div className="presentation-titre">{children}</div>
+        <div className="presentation-aside">
+          <OpinionRecipe recipeRate={recipeRate} recipeNbRate={recipeNbRate} />
+        </div>
       </div>
     </div>
   );
